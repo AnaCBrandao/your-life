7b75f79855be181805baac70587b8ef4e0ecf91f
anaBrandao <anacarolini633@gmail.com>
2022-11-30 18:05:24 -0300

    personalização

---
+++ src/Pages/AppExplanation/index.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  handleNavHome,
+  handleSetShowHome,
+} from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/ExplanationCard";
+import { useNavigation } from "@react-navigation/native";
+
+export default function AppExplanation() {
+  const navigation = useNavigation();
+
+  function handleNavHome() {
+    navigation.navigate("Home");
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.title}>
+            Antes, deixa {"\n"} eu te explicar...
+          </Text>
+          <ExplanationCard />
+          <Text style={styles.descriptionCta}>
+            Pronto(a) para subir de nível na vida?
+          </Text>
+          <Text style={styles.description}>
+            Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos de
+            forma individual.
+          </Text>
+          <DefaultButton
+            buttonText={"Continuar"}
+            handlePress={handleNavHome}
+            width={250}
+            height={50}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#7ed957",
+    textAlign: "center",
+    marginVertical: 40,
+  },
+  descriptionCta: {
+    color: "#7ed957",
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  description: {
+    color: "white",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+});
