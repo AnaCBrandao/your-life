5cf1d06372f8a8159e89d4844a99d8ca7895aeb0
anaBrandao <anacarolini633@gmail.com>
2022-12-01 22:35:14 -0300

    sqlite e serviço de SET

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -6,6 +6,8 @@ import SelectHabit from "./SelectHabit";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
 import Notification from "../../Components/HabitPage/Notification";
 import TimeDatePicker from "../../Components/HabitPage/TimeDataPicker";
+import UpdateExcludeButtons from "../../Components/HabitPage/UpdateExcludeButton";
+import DefaultButton from "../../Components/Common/DefaultButton";
 
 export default function HabitPage({ route }) {
   const navigation = useNavigation();
@@ -16,6 +18,92 @@ export default function HabitPage({ route }) {
   const [timeNotification, setTimeNotification] = useState();
 
   const {create, habit} = route.params;
+
+  function handleCreateHabit() {
+    if (habitInput === undefined || frequencyInput === undefined) {
+      Alert.alert(
+        "Você precisa selecionar um hábito e frequência para continuar"
+      );
+    } else if (
+      notificationToggle === true &&
+      frequencyInput === "Diário" &&
+      timeNotification === undefined
+    ) {
+      Alert.alert("Você precisa dizer o horário da notificação!");
+    } else if (
+      notificationToggle === true &&
+      frequencyInput === "Diário" &&
+      dayNotification === undefined &&
+      timeNotification === undefined
+    ) {
+      Alert.alert(
+        "Você precisa dizer a frequência e o horário da notificação!"
+      );
+    } else {
+      if (notificationToggle) {
+          NotificationService.createNotification(
+          habitInput,
+          frequencyInput,
+          dayNotification,
+          timeNotification
+        );
+      }
+
+      HabitsService.createHabit({
+        habitArea: habit?.habitArea,
+        habitName: habitInput,
+        habitFrequency: frequencyInput,
+        habitHasNotification: notificationToggle,
+        habitNotificationFrequency: dayNotification,
+        habitNotificationTime: timeNotification,
+        lastCheck: formatDate,
+        daysWithoutChecks: 0,
+        habitIsChecked: 0,
+        progressBar: 1,
+        habitChecks: 0,
+      }).then(() => {
+        Alert.alert("Sucesso na criação do hábito!");
+
+        navigation.navigate("Home", {
+          createdHabit: `Created in ${habit?.habitArea}`,
+        });
+      });
+    }
+  }
+
+  function handleUpdateHabit() {
+    if (notificationToggle === true && !dayNotification && !timeNotification) {
+      Alert.alert("Você precisa colocar a frequência e horário da notificação");
+    } else {
+      HabitsService.updateHabit({
+        habitArea: habit?.habitArea,
+        habitName: habitInput,
+        habitFrequency: frequencyInput,
+        habitHasNotification: notificationToggle,
+        habitNotificationFrequency: dayNotification,
+        habitNotificationTime: timeNotification,
+        habitNotificationId: notificationToggle ? habitInput : null,
+      }).then(() => {
+        Alert.alert("Sucesso na atualização do hábito");
+        if (!notificationToggle) {
+          NotificationService.deleteNotification(habit?.habitName);
+        } else {
+          NotificationService.deleteNotification(habit?.habitName);
+          NotificationService.createNotification(
+            habitInput,
+            frequencyInput,
+            dayNotification,
+            timeNotification
+          );
+        }
+
+        navigation.navigate("Home", {
+          updatedHabit: `Updated in ${habit?.habitArea}`,
+        });
+      });
+    }
+  }
+
 
   return (
     <View style={styles.container}>
@@ -65,6 +153,23 @@ export default function HabitPage({ route }) {
                 />
               )
             ) : null}
+
+            {create === false ? (
+              <UpdateExcludeButtons
+                handleUpdate={handleUpdateHabit}
+                habitArea={habit?.habitArea}
+                habitInput={habitInput}
+              />
+            ) : (
+              <View style={styles.configButton}>
+                <DefaultButton
+                  buttonText={"Criar"}
+                  handlePress={handleCreateHabit}
+                  width={250}
+                  height={50}
+                />
+              </View>
+            )}
 
           </View>
         </View>
